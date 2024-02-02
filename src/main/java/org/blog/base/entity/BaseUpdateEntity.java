463367bb5239e9c1d85cf0d714e22ffa6b4eeaf4
GinToFim <shlf6673@gmail.com>
2024-02-02 17:04:06 +0900

    feat: 베이스 엔티티 추상클래스 추가

---
+++ src/main/java/org/blog/base/entity/BaseUpdateEntity.java
@@ -0,0 +1,22 @@
+package org.blog.base.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseUpdateEntity {
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+}
